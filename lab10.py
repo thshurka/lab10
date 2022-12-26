4c780c2bc16e936134f7499c776009097fdfa86a
thshurka <114878213+thshurka@users.noreply.github.com>
2022-12-26 18:21:22 +0500

    Fixed

--- lab10.py
+++ lab10.py
@@ -2,31 +2,37 @@ import logging
 
 logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="a")
 
-col = int(input())
+col = int(input("Введите число рублей"))
 logging.info(f"Users entered: {col}")
 i = 0
+j = 0
+k = 0
+g = 0
+m = 0
+f = 0
+d = 0
 while col != 0:
     if col - 64 >= 0:
         i += 1
         col -= 64
     elif col - 32 >= 0:
-        i += 1
+        j += 1
         col -= 32
     elif col - 16 >= 0:
-        i += 1
+        k += 1
         col -= 16
     elif col - 8 >= 0:
-        i += 1
+        g += 1
         col -= 8
     elif col - 4 >= 0:
-        i += 1
+        m += 1
         col -= 4
     elif col - 2 >= 0:
-        i += 1
+        f += 1
         col -= 2
     else:
-        i += 1
+        d += 1
         col -= 1
-logging.info(f"Result: {i}")
-print(i)
+logging.info(f"Result: {i,j,k,g,m,f,d}")
+print(" Купюр по 64:",i," Купюр по 32:", j, " Купюр по 16:", k, " Купюр по 8:", g, " Купюр по 4:",m, " Купюр по 2:",f, " Купюр по 1:", d )
 logging.info("Program ended")
--- readme.md
+++ readme.md
@@ -10,4 +10,6 @@ ___
 ---
 ---
 Примеры работы программы
-![img.png](img.png)
+![img_1.png](img_1.png)
+![img_2.png](img_2.png)
+![img_3.png](img_3.png)
